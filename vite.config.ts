482017eb81c9f947a65166a85ab46e6985093655
tagerbrands <t.a.gerbrands@gmail.com>
2026-08-26 16:10:07 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/anatomy-vibe/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
